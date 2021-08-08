911489bcca6d917d7b77ef2e80234cf4c913afc8
GitHub Action <action@github.com>
2021-08-08 07:47:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/neo321654/buildplaginforidea/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.neo321654.buildplaginforidea.services
+
+import com.github.neo321654.buildplaginforidea.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/neo321654/buildplaginforidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/neo321654/buildplaginforidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.neo321654.buildplaginforidea.services
 
+import com.github.neo321654.buildplaginforidea.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
